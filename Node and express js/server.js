a76d34c353ecb3070644121bd04993eb4c6cde9b
Shawon <shawonh986@gmail.com>
2023-11-07 02:46:25 +0800

    modified

--- Node and express js/server.js
+++ Node and express js/server.js
@@ -5,13 +5,18 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 
 
+//! API to check connection
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to express app"})
+})
+
 
 
 
 const port = 7009;
 app.listen(port,()=>{
-    console.log("App is running on port 7009")
+    console.log(`App is running on port ${port} `)
 })
-
+   
 
 
